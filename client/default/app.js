6dad25671f0b92070e8726bf8c599d4e3dc0a59e
Brid Mackey <brid.mackey@gmail.com>
2014-04-25 12:09:16 +0100

    sytling to fix home page issue

--- client/default/app.js
+++ client/default/app.js
@@ -68617,236 +68617,246 @@ Ext.define('Xpoit.view.Home', {
 
             {
                 xtype: 'panel',
-                height: 60,
+                height: 30,
             },
 
-            //search
             {
-                height: 45,
-
+                id: 'homeMenu',
                 layout: {
-                    type: 'hbox',
-                    pack: 'center',
+                    type: 'vbox'
                 },
 
-                items: [{
-                        flex: 1,
-                    }, {
-                        flex: 1.5,
-                        html: '<img class="icons" src="resources/images/icons/search.png" />'
-                    }, {
-                        id: 'searchBtn',
-                        cls: 'homeIcons',
-                        height: 10,
-                        width: 200,
-                        html: 'Search<br /><hr />',
+                items: [ //search
+                    {
+                        height: 45,
 
-                        // listeners: {
-                        //     element: 'element',
-                        //     tap: function() {
-                        //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Search'));
-                        //     }
-                        // }
-                    }, {
-                        flex: 1
-                    }
+                        layout: {
+                            type: 'hbox',
+                            pack: 'center',
+                        },
 
-                ]
-            },
+                        items: [{
+                                flex: 1,
+                            }, {
+                                flex: 1.5,
+                                html: '<img class="icons" src="resources/images/icons/search.png" />'
+                            }, {
+                                id: 'searchBtn',
+                                cls: 'homeIcons',
+                                height: 10,
+                                width: 200,
+                                html: 'Search<br /><hr />',
 
-            // Student Link
-            {
-                height: 45,
-
-                layout: {
-                    type: 'hbox',
-                    pack: 'center',
-                },
-                items: [{
-                        flex: 1,
-                    }, {
-                        flex: 1.5,
-                        html: '<img class="icons" src="resources/images/icons/student.png" />'
-                    }, {
-                        id: 'studentBtn',
-                        cls: 'homeIcons',
-                        height: 10,
-                        width: 200,
-                        html: '<a>Student List</a><br /><hr />',
-
-                        listeners: {
-                            element: 'element',
-                            tap: function() {
-                                Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Main'));
+                                // listeners: {
+                                //     element: 'element',
+                                //     tap: function() {
+                                //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Search'));
+                                //     }
+                                // }
+                            }, {
+                                flex: 1
                             }
-                        }
-                    }, {
-                        flex: 1
-                    }
+
+                        ]
+                    },
+
+                    // Student Link
+                    {
+                        height: 45,
+
+                        layout: {
+                            type: 'hbox',
+                            pack: 'center',
+                        },
+                        items: [{
+                                flex: 1,
+                            }, {
+                                flex: 1.5,
+                                html: '<img class="icons" src="resources/images/icons/student.png" />'
+                            }, {
+                                id: 'studentBtn',
+                                cls: 'homeIcons',
+                                height: 10,
+                                width: 200,
+                                html: '<a>Student List</a><br /><hr />',
+
+                                listeners: {
+                                    element: 'element',
+                                    tap: function() {
+                                        Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Main'));
+                                    }
+                                }
+                            }, {
+                                flex: 1
+                            }
+
+                        ]
+                    },
+                    //Project Link
+                    {
+                        height: 45,
+
+                        layout: {
+                            type: 'hbox',
+                            pack: 'center',
+                        },
+                        items: [{
+                            flex: 1,
+                        }, {
+                            flex: 1.5,
+                            html: '<img class="icons" src="resources/images/icons/project.png" />'
+                        }, {
+                            id: 'projectBtn',
+                            cls: 'homeIcons',
+                            height: 10,
+                            width: 200,
+                            html: '<a>Project List</a><br /><hr />',
+
+                            // listeners: {
+                            //     element: 'element',
+                            //     tap: function() {
+                            //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Project'));
+                            //     }
+                            // }
+                        }, {
+                            flex: 1
+                        }]
+                    },
+
+                    //Maps Link
+                    {
+                        height: 45,
+
+                        layout: {
+                            type: 'hbox',
+                            pack: 'center',
+                        },
+                        items: [{
+                            flex: 1,
+                        }, {
+                            flex: 1.5,
+                            html: '<img class="icons" src="resources/images/icons/map.png" />'
+                        }, {
+                            id: 'mapBtn',
+                            cls: 'homeIcons',
+                            height: 10,
+                            width: 200,
+                            html: '<a>Location Maps</a><br /><hr />',
+
+                            // listeners: {
+                            //     element: 'element',
+                            //     tap: function() {
+                            //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Maps'));
+                            //     }
+                            // }
+                        }, {
+                            flex: 1
+                        }]
+                    },
+
+                    //Visit It Link
+                    {
+                        height: 45,
+
+                        layout: {
+                            type: 'hbox',
+                            pack: 'center',
+                        },
+                        items: [{
+                            flex: 1,
+                        }, {
+                            flex: 1.5,
+                            html: '<img class="icons" src="resources/images/icons/visit.png" />'
+                        }, {
+                            id: 'visitBtn',
+                            cls: 'homeIcons',
+                            height: 10,
+                            width: 200,
+                            html: '<a>VisitIt!</a><br /><hr />',
+
+                            // listeners: {
+                            //     element: 'element',
+                            //     tap: function() {
+                            //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Visit'));
+                            //     }
+                            // }
+                        }, {
+                            flex: 1
+                        }]
+                    },
+
+
+                    //NoteIt Link
+                    {
+                        height: 45,
+
+                        layout: {
+                            type: 'hbox',
+                            pack: 'center',
+                        },
+                        items: [{
+                            flex: 1,
+                        }, {
+                            flex: 1.5,
+                            html: '<img class="icons" src="resources/images/icons/note.png" />'
+                        }, {
+                            id: 'noteBtn',
+                            cls: 'homeIcons',
+                            height: 10,
+                            width: 200,
+                            html: '<a>NoteIt! Microblog</a><br /><hr />',
+
+                            listeners: {
+                                element: 'element',
+                                tap: function() {
+
+                                    Ext.Msg.confirm("External Url", "Are you sure you want to leave the app?", function(btn) {
+                                        if (btn == 'yes') {
+                                            window.open('https://pure-badlands-7549.herokuapp.com/home'); // which page wants to redirect
+                                        }
+                                    });
+                                }
+                            }
+                        }, {
+                            flex: 1
+                        }]
+                    },
+
+
+                    //Info Link
+                    {
+                        height: 45,
+
+                        layout: {
+                            type: 'hbox',
+                            pack: 'center',
+                        },
+                        items: [{
+                            flex: 1,
+                        }, {
+                            flex: 1.5,
+                            html: '<img class="icons" src="resources/images/icons/info.png" />'
+                        }, {
+                            id: 'infoBtn',
+                            cls: 'homeIcons',
+                            height: 10,
+                            width: 200,
+                            html: '<a>Information</a>',
+
+                            // listeners: {
+                            //     element: 'element',
+                            //     tap: function() {
+                            //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Info'));
+                            //     }
+                            // }
+                        }, {
+                            flex: 1
+                        }]
+                    },
 
                 ]
             },
-            //Project Link
-            {
-                height: 45,
 
-                layout: {
-                    type: 'hbox',
-                    pack: 'center',
-                },
-                items: [{
-                    flex: 1,
-                }, {
-                    flex: 1.5,
-                    html: '<img class="icons" src="resources/images/icons/project.png" />'
-                }, {
-                    id: 'projectBtn',
-                    cls: 'homeIcons',
-                    height: 10,
-                    width: 200,
-                    html: '<a>Project List</a><br /><hr />',
-
-                    // listeners: {
-                    //     element: 'element',
-                    //     tap: function() {
-                    //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Project'));
-                    //     }
-                    // }
-                }, {
-                    flex: 1
-                }]
-            },
-
-            //Maps Link
-            {
-                height: 45,
-
-                layout: {
-                    type: 'hbox',
-                    pack: 'center',
-                },
-                items: [{
-                    flex: 1,
-                }, {
-                    flex: 1.5,
-                    html: '<img class="icons" src="resources/images/icons/map.png" />'
-                }, {
-                    id: 'mapBtn',
-                    cls: 'homeIcons',
-                    height: 10,
-                    width: 200,
-                    html: '<a>Location Maps</a><br /><hr />',
-
-                    // listeners: {
-                    //     element: 'element',
-                    //     tap: function() {
-                    //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Maps'));
-                    //     }
-                    // }
-                }, {
-                    flex: 1
-                }]
-            },
-
-            //Visit It Link
-            {
-                height: 45,
-
-                layout: {
-                    type: 'hbox',
-                    pack: 'center',
-                },
-                items: [{
-                    flex: 1,
-                }, {
-                    flex: 1.5,
-                    html: '<img class="icons" src="resources/images/icons/visit.png" />'
-                }, {
-                    id: 'visitBtn',
-                    cls: 'homeIcons',
-                    height: 10,
-                    width: 200,
-                    html: '<a>VisitIt!</a><br /><hr />',
-
-                    // listeners: {
-                    //     element: 'element',
-                    //     tap: function() {
-                    //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Visit'));
-                    //     }
-                    // }
-                }, {
-                    flex: 1
-                }]
-            },
-
-
-            //NoteIt Link
-            {
-                height: 45,
-
-                layout: {
-                    type: 'hbox',
-                    pack: 'center',
-                },
-                items: [{
-                    flex: 1,
-                }, {
-                    flex: 1.5,
-                    html: '<img class="icons" src="resources/images/icons/note.png" />'
-                }, {
-                    id: 'noteBtn',
-                    cls: 'homeIcons',
-                    height: 10,
-                    width: 200,
-                    html: '<a>NoteIt! Microblog</a><br /><hr />',
-
-                    listeners: {
-                        element: 'element',
-                        tap: function() {
-
-                            Ext.Msg.confirm("External Url", "Are you sure you want to leave the app?", function(btn) {
-                                if (btn == 'yes') {
-                                    window.open('https://pure-badlands-7549.herokuapp.com/home'); // which page wants to redirect
-                                }
-                            });
-                        }
-                    }
-                }, {
-                    flex: 1
-                }]
-            },
-
-
-            //Info Link
-            {
-                height: 45,
-
-                layout: {
-                    type: 'hbox',
-                    pack: 'center',
-                },
-                items: [{
-                    flex: 1,
-                }, {
-                    flex: 1.5,
-                    html: '<img class="icons" src="resources/images/icons/info.png" />'
-                }, {
-                    id: 'infoBtn',
-                    cls: 'homeIcons',
-                    height: 10,
-                    width: 200,
-                    html: '<a>Information</a>',
-
-                    // listeners: {
-                    //     element: 'element',
-                    //     tap: function() {
-                    //         Ext.Viewport.setActiveItem(Ext.create('Xpoit.view.Info'));
-                    //     }
-                    // }
-                }, {
-                    flex: 1
-                }]
-            },
 
         ]
     }
